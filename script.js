87eb41f06533b8592959446d395173c52d86b2c0
Darrell VanFleet <dasmvanfleet@msn.com>
2017-10-14 18:47:14 -0600

    checkerboard part 2

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// Your JS goes here
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+
+
+  for (var i = 0; i < 9; i++){
+    //create a new array
+    for (var j = 0; j < 9; j++){
+    //create a div to put in an array space
+    var chkrBrd = document.createElement("div");
+    var r = getRandomInt(0,255);
+    var g = getRandomInt(0,255);
+    var b = getRandomInt(0,255);
+    var randColor = `rgb(${r},${g},${b})`;
+        chkrBrd.style.backgroundColor = randColor;
+
+        chkrBrd.style.width = "11.1%";
+        chkrBrd.style.float = "left";
+        chkrBrd.style.paddingBottom = "11.1%";
+      document.body.appendChild(chkrBrd);
+
+    }
+
+  }
